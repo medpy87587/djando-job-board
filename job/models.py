dca65dac0372a93c843b0fb580931a8a628307d3
medpy87587 <medamisah@gmail.com>
2022-10-08 21:09:49 +0100

    add app job

--- job/models.py
+++ job/models.py
@@ -1,3 +1,7 @@
+from turtle import title
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class job(models.Model):
+    title=models.CharField(max_length=100)
